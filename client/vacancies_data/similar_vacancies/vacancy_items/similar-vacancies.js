5c291205dc41867982ac1d67105904859b9c7c10
Alexander Shmidt <shmidt.alex@gmail.com>
2017-04-17 15:58:17 +0300

    adding git to project folder

---
+++ client/vacancies_data/similar_vacancies/vacancy_items/similar-vacancies.js
@@ -0,0 +1,154 @@
+
+var vacanciesData =[
+	{
+		src: 'https://habrastorage.org/getpro/moikrug/uploads/company/100/005/419/5/logo/medium_dbfccb9b1f899bf2660c00d6df74dece.png',
+		title:"Frontend-разработчик",
+		name:'ЗАО "Инфорус"',
+		location:"Москва",
+		salary:"От 90 000 до 110 000 руб."
+	},
+	{
+		src: 'https://habrastorage.org/getpro/moikrug/uploads/company/100/005/419/5/logo/medium_dbfccb9b1f899bf2660c00d6df74dece.png',
+		title:"Frontend-разработчик",
+		name:'ЗАО "Инфорус"',
+		location:"Москва",
+		salary:"От 90 000 до 110 000 руб."
+	}
+]
+
+Template.similarVacancies.helpers({
+	vacancy:vacanciesData
+});
+/*var vacanciesData = [iconData = [
+	{
+		src:'https://habrastorage.org/getpro/moikrug/uploads/company/100/005/419/5/logo/medium_dbfccb9b1f899bf2660c00d6df74dece.png',
+		url:"https://moikrug.ru/vacancies/1000032599"
+	},
+	{
+		src:'https://habrastorage.org/getpro/moikrug/uploads/company/100/004/381/2/logo/medium_9450dc89d2da8e447e32f1a4154719d9.png',
+		url:"https://moikrug.ru/vacancies/1000032234"
+	},
+	{
+		src:"https://habrastorage.org/getpro/moikrug/uploads/company/100/005/559/4/logo/medium_6561583f79594377818fb685c52bb0aa.png",
+		url:"https://moikrug.ru/vacancies/1000032474"
+	},
+	{
+		src:"https://habrastorage.org/getpro/moikrug/uploads/company/100/004/693/8/logo/medium_ab777d5b17c34f957b83f47e2e8c5d04.jpg",
+		url:"https://moikrug.ru/vacancies/1000029931"
+	},
+	{
+		src:"https://habrastorage.org/getpro/moikrug/uploads/company/100/005/635/4/logo/medium_d250bda60a0accdb04affc900c4e015e.png",
+		url:"https://moikrug.ru/vacancies/1000032623"
+	},
+	{
+		src:"https://habrastorage.org/getpro/moikrug/uploads/company/100/004/693/5/logo/medium_0058dc95ce39fb906ea4dd0b4606ea8a.png",
+		url:"https://moikrug.ru/vacancies/1000032297"
+	}
+],
+titleData = [
+	{
+		title:"Frontend-разработчик",
+		url:"https://moikrug.ru/vacancies/1000032599"
+	},
+	{
+		title:"Senior frontend developer",
+		url:"https://moikrug.ru/vacancies/1000032234"
+	},
+	{
+		src:"JavaScript Frontend/Backend разработчик (react, redux, node.js, sql)",
+		url:"https://moikrug.ru/vacancies/1000027132"
+	},
+	{
+		title:"Frontend-разработчик",
+		url:"https://moikrug.ru/vacancies/1000029931"
+	},
+	{
+		title:"Frontend-разработчик",
+		url:"https://moikrug.ru/vacancies/1000032474"
+	},
+	{
+		title:"🚀 Ведущий front-end разработчик",
+		url:""
+	}	
+],
+nameData = [
+	{
+		name:'ЗАО "Инфорус"',
+		url:"https://moikrug.ru/companies/inforus"
+	},
+	{
+		name:"Digital Education Technology",
+		url:"https://moikrug.ru/companies/digitaledtech"
+	},	
+	{
+		name:'ООО "ДиджиРиал"',
+		url:"https://moikrug.ru/companies/c15a0950-f57d-4357-b575-adcbf3ae1dc3"
+	},
+	{
+		name:"МЛСН.ру",
+		url:"https://moikrug.ru/companies/mlsnru"
+	},
+	{
+		name:"Acquired IO",
+		url:"https://moikrug.ru/companies/acquired-io"
+	},
+	{
+		name:"Leningrad Agency",
+		url:"https://moikrug.ru/companies/leningrad"
+	}
+],				
+locationData = [
+	{
+		location:"Москва",
+		url:"https://moikrug.ru/vacancies?city_id=678"
+	},
+	{
+		location:"Санкт-Петербург",
+		url:"https://moikrug.ru/vacancies?city_id=679"
+	},
+	{
+		location:"Краснодар",
+		url:"https://moikrug.ru/vacancies?city_id=707"
+	},
+	{
+		location:"",
+		url:""
+	},
+	{
+		location:"Омск",
+		url:"https://moikrug.ru/vacancies?city_id=718"
+	},
+	{
+		location:"Москва",
+		url:"https://moikrug.ru/vacancies?city_id=678"
+	}
+],					
+salaryData =[
+	{
+		salary:"От 90 000 до 110 000 руб."
+	},
+	{
+		salary:"От 100 000  руб."
+	},
+	{
+		salary:"От 80 000 до 120 000 руб."
+	},
+	{
+		salary:"От 80 000 до 85 000 руб."
+	},
+	{
+		salary:"От 140 000 руб."
+	},
+	{
+		salary:"от 90 000 руб."
+	}	
+]]
+
+Template.vacancyItem.helpers({
+	
+	icon: iconData,
+	title:titleData,
+	name:nameData,
+	location:locationData,
+	salary:salaryData
+});*/
